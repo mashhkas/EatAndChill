7041cab6c9c6df5b6e5554c07ee805ee5dc10d8c
mashhkas <marichkamelnik380@gmail.com>
2026-05-04 02:10:33 +0300

    Remove build folder

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/EatAndChill.dir/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/rcc/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/EatAndChill.dir/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/rcc/qrc_resources.cpp.obj: \
- C:\QtProjects\EatAndChill\build\Desktop_Qt_6_11_0_MinGW_64_bit-Debug\.qt\rcc\qrc_resources.cpp
